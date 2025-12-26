d6c8e86ae99c4d7150202cad29b1c1405f89e7dd
Jens Eriksson <Oftatkofta@users.noreply.github.com>
2025-12-26 23:36:19 +0100

    split data into topics

--- bme680-sensor-mqtt.py
+++ bme680-sensor-mqtt.py
@@ -2,7 +2,6 @@ import bme680 as bme680
 import paho.mqtt.client as mqtt
 import time
 import os
-import json
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
@@ -21,13 +20,13 @@ sensor.set_filter(bme680.FILTER_SIZE_0)
 
 # Set the gas sensor heater temperature
 sensor.set_gas_heater_temperature(320)
-sensor.set_gas_heater_duration(50)
+sensor.set_gas_heater_duration(100)
 
 # setup mqtt client with the mqtt-user on Home Assistant and password from the .env file
 mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 mqtt_host = os.getenv("MQTT_HOST")
 mqtt_port = int(os.getenv("MQTT_PORT", 1883))  # Default to 1883 if not set
-mqtt_topic = os.getenv("MQTT_TOPIC")
+mqtt_base_topic = "/sensors/skjulet"
 mqtt_client.username_pw_set(username=os.getenv("MQTT_USER"), password=os.getenv("MQTT_PASSWORD"))
 mqtt_client.connect(mqtt_host, mqtt_port, 60)
 
@@ -38,13 +37,11 @@ while True:
     humidity = sensor.data.humidity
     gas = sensor.data.gas_resistance
 
-    # Publish sensor data to MQTT
-    mqtt_client.publish(topic=mqtt_topic, payload=json.dumps({
-        "temperature": temperature,
-        "pressure": pressure,
-        "humidity": humidity,
-        "gas": gas
-    }))
+    # Publish sensor data to individual MQTT topics
+    mqtt_client.publish(topic=f"{mqtt_base_topic}/temperature_c", payload=str(temperature))
+    mqtt_client.publish(topic=f"{mqtt_base_topic}/pressure_pa", payload=str(pressure))
+    mqtt_client.publish(topic=f"{mqtt_base_topic}/humidity_percent", payload=str(humidity))
+    mqtt_client.publish(topic=f"{mqtt_base_topic}/gas_ohms", payload=str(gas))
 
     print(f"Temperature: {temperature} C")
     print(f"Pressure: {pressure} Pa")
