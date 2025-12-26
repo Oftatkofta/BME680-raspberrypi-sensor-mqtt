1b771ceba0951f609096e706f9a79dad47a0d6e8
Jens Eriksson <Oftatkofta@users.noreply.github.com>
2025-12-26 21:07:00 +0100

    debug

--- bme680-sensor-mqtt.py
+++ bme680-sensor-mqtt.py
@@ -23,29 +23,13 @@ sensor.set_filter(bme680.FILTER_SIZE_0)
 sensor.set_gas_heater_temperature(320)
 sensor.set_gas_heater_duration(50)
 
-sensor.get_sensor_data()
-temperature = sensor.data.temperature
-pressure = sensor.data.pressure
-humidity = sensor.data.humidity
-gas = sensor.data.gas_resistance
-
 # setup mqtt client with the mqtt-user on Home Assistant and password from the .env file
 mqtt_client = mqtt.Client()
+mqtt_host = os.getenv("MQTT_HOST")
+mqtt_port = int(os.getenv("MQTT_PORT", 1883))  # Default to 1883 if not set
+mqtt_topic = os.getenv("MQTT_TOPIC")
 mqtt_client.username_pw_set(username=os.getenv("MQTT_USER"), password=os.getenv("MQTT_PASSWORD"))
-mqtt_client.connect(os.getenv("MQTT_HOST"), os.getenv("MQTT_PORT"), 60)
-
-mqtt_client.publish(topic=os.getenv("MQTT_TOPIC"), payload=json.dumps({
-    "temperature": temperature,
-    "pressure": pressure,
-    "humidity": humidity,
-    "gas": gas
-}))
-
-
-print(f"Temperature: {temperature} C")
-print(f"Pressure: {pressure} Pa")
-print(f"Humidity: {humidity} %")
-print(f"Gas: {gas} Ohms")
+mqtt_client.connect(mqtt_host, mqtt_port, 60)
 
 while True:
     sensor.get_sensor_data()
@@ -53,6 +37,14 @@ while True:
     pressure = sensor.data.pressure
     humidity = sensor.data.humidity
     gas = sensor.data.gas_resistance
+
+    # Publish sensor data to MQTT
+    mqtt_client.publish(topic=mqtt_topic, payload=json.dumps({
+        "temperature": temperature,
+        "pressure": pressure,
+        "humidity": humidity,
+        "gas": gas
+    }))
 
     print(f"Temperature: {temperature} C")
     print(f"Pressure: {pressure} Pa")
